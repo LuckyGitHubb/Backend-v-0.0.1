a5a669c479eb02244305bef89cac5e0e3197f098
lucky <singh22.lakhi@gmail.com>
2026-03-04 21:00:36 +0530

    add route for the invoice payment stats api

--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getDashboardCounts } = require('../controller/dashboardController');
+const { getDashboardCounts, getInvoicePaymentStats } = require('../controller/dashboardController');
 
 const router = express.Router();
 
 router.get('/counts', getDashboardCounts);
+router.get('/invoice/payment/stats', getInvoicePaymentStats);
 
 module.exports = router;
