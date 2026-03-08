900adb412311d2996033f47b7737eb573f20ab23
lucky <singh22.lakhi@gmail.com>
2026-03-09 02:06:01 +0530

    add two routes for the stats APIs

--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { getDashboardCounts, getInvoicePaymentStats } = require('../controller/dashboardController');
+const { getDashboardCounts, getInvoicePaymentStats, getMonthlySalesStats, getStockStatusStats } = require('../controller/dashboardController');
 
 const router = express.Router();
 
 router.get('/counts', getDashboardCounts);
 router.get('/invoice/payment/stats', getInvoicePaymentStats);
+router.get('/sale/monthly/stats', getMonthlySalesStats);
+router.get('/stock/status/stats', getStockStatusStats);
 
 module.exports = router;
